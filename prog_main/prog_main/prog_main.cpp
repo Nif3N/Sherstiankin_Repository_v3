0f551dd7cffa093c8e337684de88e8e84460552d
Sherstiankin Nikita <nik.sherstyankin.75@mail.ru>
2024-04-17 21:20:35 +0300

    added_fun_4

--- prog_main/prog_main/prog_main.cpp
+++ prog_main/prog_main/prog_main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 
 using namespace std;
@@ -17,30 +16,41 @@ void MultiplyN(int num_X, int num_N)
 }
 void CountNumX(int num_X)
 {
-
+    int count = 1;
+    while (num_X / 10 != 0)
+    {
+        count++;
+        num_X = num_X / 10;
+    }
+    cout << "Кол-во цифр в числе X: " << count;
 }
 
 int main()
 {
-    cout << "1. Ввести натуральное число X";
-    cout << "2. Ввести цифру N (которая меньше числа разрядов числа X)";
-    cout << "3. Определить произведение последних N цифр натурального числа X";
-    cout << "4. Определить количество цифр введенного числа X";
+    setlocale(LC_ALL, "rus");
+    cout << "1. Ввести натуральное число X" << endl;
+    cout << "2. Ввести цифру N (которая меньше числа разрядов числа X)" << endl;
+    cout << "3. Определить произведение последних N цифр натурального числа X" << endl;
+    cout << "4. Определить количество цифр введенного числа X" << endl;
     cout << "Введите номер функции (1-4) или 0 для выхода: ";
-
     int k, x, n;
     cin >> k;
     switch (k) {
     case 1: 
         EnterX(x);
+        break;
     case 2:
         EnterN(n);
+        break;
     case 3: 
         MultiplyN(x, n);
+        break;
     case 4:
         CountNumX(x);
+        break;
     default:
         cout << "Ошибка ввода!";
+        break;
     }
 }
 
