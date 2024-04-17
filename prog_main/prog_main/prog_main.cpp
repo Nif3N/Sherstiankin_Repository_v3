cf269ddef737ec230b51ba4ba2a5ab3a5118603d
Sherstiankin Nikita <nik.sherstyankin.75@mail.ru>
2024-04-17 21:05:00 +0300

    added_fun_3

--- prog_main/prog_main/prog_main.cpp
+++ prog_main/prog_main/prog_main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 
 using namespace std;
@@ -13,7 +12,15 @@ void EnterN(int &num_N)
 }
 void MultiplyN(int num_X, int num_N)
 {
-
+    int res = 1;
+    int mult = 0;
+    for (int i = 0; i < num_N; i++)
+    {
+        mult = num_X % 10;
+        num_X = num_X / 10;
+        res = res * mult;
+    }
+    cout << "Произведение последних N цифр: " << res << endl;
 }
 void CountNumX(int num_X)
 {
@@ -22,25 +29,30 @@ void CountNumX(int num_X)
 
 int main()
 {
-    cout << "1. Ввести натуральное число X";
-    cout << "2. Ввести цифру N (которая меньше числа разрядов числа X)";
-    cout << "3. Определить произведение последних N цифр натурального числа X";
-    cout << "4. Определить количество цифр введенного числа X";
+    setlocale(LC_ALL, "rus");
+    cout << "1. Ввести натуральное число X" << endl;
+    cout << "2. Ввести цифру N (которая меньше числа разрядов числа X)" << endl;
+    cout << "3. Определить произведение последних N цифр натурального числа X" << endl;
+    cout << "4. Определить количество цифр введенного числа X" << endl;
     cout << "Введите номер функции (1-4) или 0 для выхода: ";
-
     int k, x, n;
     cin >> k;
     switch (k) {
     case 1: 
         EnterX(x);
+        break;
     case 2:
         EnterN(n);
+        break;
     case 3: 
         MultiplyN(x, n);
+        break;
     case 4:
         CountNumX(x);
+        break;
     default:
         cout << "Ошибка ввода!";
+        break;
     }
 }
 
