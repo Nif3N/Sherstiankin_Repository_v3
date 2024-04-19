bc78dd9140eeed12f9d8b6bc2230f49ad16d97f1
Sherstiankin Nikita <nik.sherstyankin.75@mail.ru>
2024-04-19 20:23:10 +0300

    Resolve merge conflict

--- prog_main/prog_main/prog_main.cpp
+++ prog_main/prog_main/prog_main.cpp
@@ -1,11 +1,13 @@
-
 #include <iostream>
 
 using namespace std;
 
 void EnterX(int &num_X)
 {
-
+    do {
+        cout << "¬ведите X:";
+        cin >> num_X;
+    } while (num_X <= 0);
 }
 void EnterN(int &num_N, int &num_X)
 {
@@ -25,6 +27,10 @@ void CountNumX(int num_X)
 
 int main()
 {
+<<<<<<< HEAD
+=======
+    setlocale(LC_ALL, "rus");
+>>>>>>> 969c4fe1bc45bbfe24f07870ada95b5c9e14ea0b
     cout << "1. ¬вести натуральное число X" << endl;
     cout << "2. ¬вести цифру N (котора€ меньше числа разр€дов числа X)" << endl;
     cout << "3. ќпределить произведение последних N цифр натурального числа X" << endl;
@@ -37,7 +43,11 @@ int main()
         EnterX(x);
         break;
     case 2:
+<<<<<<< HEAD
         EnterN(n, x);
+=======
+        EnterN(n);
+>>>>>>> 969c4fe1bc45bbfe24f07870ada95b5c9e14ea0b
         break;
     case 3: 
         MultiplyN(x, n);
